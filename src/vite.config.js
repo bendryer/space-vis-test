029f4c2c29cf444ff2ffdcac45fd349529d93c32
Ben Dryer <ben.dryer@gmail.com>
2025-12-08 13:23:10 +0000

    hello world fix

---
+++ src/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // REPLACE 'solar-system-explorer' WITH YOUR REPO NAME
+  // Example: If your repo is at https://github.com/bendryer/solar-system-explorer
+  // the base should be '/solar-system-explorer/'
+  base: '/space-vis-test/'
+})
